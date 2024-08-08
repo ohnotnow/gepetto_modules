e8dadbd731d33920d22a01dfb885c4b36bf3a5db
Ohffs <ohffsnotnow@gmail.com>
2024-08-08 14:12:02 +0100

    Actually fix them...

--- gepetto/gpt.py
+++ gepetto/gpt.py
@@ -39,7 +39,7 @@ class GPTModel():
             return round(token_price_input * token_count, 4)
         return round(token_price_output * token_count, 4)
 
-    async def chat(self, messages, temperature=1.8, model=None, top_p=0.6):
+    async def chat(self, messages, temperature=1.0, model=None, top_p=0.6):
         """Chat with the model.
 
         Args:
